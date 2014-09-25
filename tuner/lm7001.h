2f7e5bdefc1e57246a409a73e7a19e51f748dfdc
WiseLord <wiselord1983@gmail.com>
2014-09-25 09:48:52 +0300

    Fix LM7001 control lines.

--- tuner/lm7001.h
+++ tuner/lm7001.h
@@ -8,8 +8,8 @@
 #define LM7001_PIN			PINC
 
 #define LM7001_DATA			(1<<PC2)
-#define LM7001_CL			(1<<PC3)
-#define LM7001_CE			(1<<PC4)
+#define LM7001_CL			(1<<PC1)
+#define LM7001_CE			(1<<PC0)
 
 /* IF = 10.7MHz and step = 100kHz */
 #define LM7001_IF			1070
