16bdd147d36b60ebe3d2cd3b9937a22c7690a1ec
WiseLord <wiselord1983@gmail.com>
2014-10-03 17:49:32 +0300

    Code refactoring. Change input values.

--- input.h
+++ input.h
@@ -10,13 +10,13 @@
 #define ENC_AB			0b11
 
 #define BTN_STATE_0		0b00000
-#define BTN_1			0b00001
-#define BTN_2			0b00010
+#define BTN_1			0b10000
+#define BTN_2			0b01000
 #define BTN_3			0b00100
-#define BTN_4			0b01000
-#define BTN_5			0b10000
-#define BTN_12			0b00011
-#define BTN_13			0b00101
+#define BTN_4			0b00010
+#define BTN_5			0b00001
+#define BTN_12			0b11000
+#define BTN_13			0b10100
 
 /* RC5 support definitions */
 #define RC5_CMD_COUNT	31
