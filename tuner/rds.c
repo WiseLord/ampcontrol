54a623988d25198dae7249d041b2c8ed4dee9e8d
WiseLord <wiselord1983@gmail.com>
2016-07-23 23:37:20 +0300

    RDS code refactoring

--- tuner/rds.c
+++ tuner/rds.c
@@ -1,6 +1,6 @@
 #include "rds.h"
 
-static char rdsText[] = "        ";
+static char rdsText[9];
 static uint8_t rdsFlag = 0;
 
 char *rdsGetText(void)
@@ -15,12 +15,23 @@ void rdsSetBlocks(uint8_t *rdsBlock)
 	// rdsBlock[4..5] - RDS block C
 	// rdsBlock[6..7] - RDS block D
 
-	if (rdsBlock[6] >= 0x20 && rdsBlock[6] < 0xFF)
-		rdsText[(rdsBlock[3] & 0x03) * 2] = rdsBlock[6];
-	if (rdsBlock[7] >= 0x20 && rdsBlock[7] < 0xFF)
-		rdsText[(rdsBlock[3] & 0x03) * 2 + 1] = rdsBlock[7];
+	uint8_t i;
+	char rdsChar;
 
-	rdsFlag = 1;
+	uint8_t rdsVersion = (rdsBlock[2] & 0x08) >> 3;
+	uint8_t rdsGroup   = (rdsBlock[2] & 0xF0) >> 4;
+	uint8_t rdsIndex   = (rdsBlock[3] & 0x03) >> 0;
+
+	if (rdsVersion == (0x00 & 0x08)) {						// RDS version = 0
+		if (rdsGroup == (0x00 & 0xF0)) {					// RDS group = 0 (RDS0)
+			for (i = 0; i < 2; i++) {
+				rdsChar = rdsBlock[6 + i];
+				if (rdsChar >= 0x20 && rdsChar < 0x80)
+					rdsText[rdsIndex * 2 + i] = rdsChar;
+			}
+			rdsFlag = 1;
+		}
+	}
 
 	return;
 }
