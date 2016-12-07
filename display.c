5dd6006aa82736f2afbb483c9801ef2d70072a26
WiseLord <wiselord1983@gmail.com>
2016-12-07 20:54:56 +0300

    Fix FM scale on graphic 128x64 displays on high frequencies

--- display.c
+++ display.c
@@ -517,7 +517,7 @@ static void showBar(int16_t min, int16_t max, int16_t value)
 	uint8_t color;
 
 	if (min + max) {
-		value = (int16_t)91 * (value - min) / (max - min);
+		value = (uint16_t)91 * (value - min) / (max - min);
 	} else {
 		value = (int16_t)45 * value / max;
 	}
