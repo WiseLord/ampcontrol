bf130f5a81b7f54a880de8f17685fab364ac2822
WiseLord <wiselord1983@gmail.com>
2017-11-18 18:19:04 +0300

    New extFunc option in editor

--- editor/mainwindow.cpp
+++ editor/mainwindow.cpp
@@ -954,7 +954,7 @@ void MainWindow::setAdcright(int value)
 
 void MainWindow::setExtfunc(int value)
 {
-    if (value >= 2)
+    if (value >= cbxExtfunc->count())
         value = 0;
     eep[EEPROM_EXT_FUNC] = (char)value;
     updateHexTable(EEPROM_EXT_FUNC);
