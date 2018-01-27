0222d053a2aca5ce674ea17c9db726075ef62b5c
WiseLord <wiselord1983@gmail.com>
2018-01-27 12:29:22 +0300

    Remote code refactoring

--- remote.c
+++ remote.c
@@ -5,268 +5,276 @@
 #include <avr/pgmspace.h>
 #include "pins.h"
 
-static volatile IRData irData;						// Last decoded IR command
-static volatile uint8_t ovfCnt = 250;				// Overflow counter
+static volatile IRData irData;                      // Last decoded IR command
+static volatile uint8_t ovfCnt = 250;               // Overflow counter
 
 void rcInit(void)
 {
-	MCUCR |= (1<<ISC10);							// Set INT1 to trigger on any edge
-	DDR(RC) &= ~RC_LINE;							// Set PD3 (INT1) to input
-	TCCR1A = 0;										// Reset Timer1 counter
-	TCCR1B = (1<<CS11) | (1<<CS10);					// Set Timer1 prescaler to 64 (250kHz)
-	GICR |= (1<<INT1);								// Enable INT3 interrupt
-
-	TIMSK = (1<<TOIE1);								// Enable Timer1 overflow interrupt
-
-	return;
+    DDR(RC) &= ~RC_LINE;                            // Set PD3 (INT1) to input
+    TCCR1A = 0;                                     // Reset Timer1 counter
+    TCCR1B = (1 << CS11) | (1 << CS10);             // Set Timer1 prescaler to 64 (250kHz)
+#ifdef _atmega32
+    MCUCR |= (1 << ISC10);                          // Set INT1 to trigger on any edge
+    GICR |= (1 << INT1);                            // Enable INT1 interrupt
+    TIMSK = (1 << TOIE1);                           // Enable Timer1 overflow interrupt
+#else
+    EICRA |= (1 << ISC00);                          // Set INT0 to trigger on any edge
+    EIMSK |= (1 << INT0);                           // Enable INT0 interrupt
+    TIMSK1 = (1 << TOIE1);                          // Enable Timer1 overflow interrupt
+#endif
+    return;
 }
 
-ISR(TIMER1_OVF_vect)								// Overflow every 1/(250kHz/65536) = 262ms
+ISR(TIMER1_OVF_vect)                                // Overflow every 1/(250kHz/65536) = 262ms
 {
-	if (ovfCnt <= 250)
-		ovfCnt++;
+    if (ovfCnt <= 250)
+        ovfCnt++;
 
-	return;
+    return;
 }
 
+#ifdef _atmega32
 ISR(INT1_vect)
+#else
+ISR(INT0_vect)
+#endif
 {
-	static uint16_t tcnt = 0;
-	uint16_t delay = TCNT1 - tcnt;					// Delay since previous interrupt
+    static uint16_t tcnt = 0;
+    uint16_t delay = TCNT1 - tcnt;                  // Delay since previous interrupt
 
-	tcnt = TCNT1;
+    tcnt = TCNT1;
 
-	// RC pin state on interrupt event is inverted due to inverted IR receiver polarity
-	uint8_t rcPin = !(PIN(RC) & RC_LINE);
+    // RC pin state on interrupt event is inverted due to inverted IR receiver polarity
+    uint8_t rcPin = !(PIN(RC) & RC_LINE);
 
-	// NEC protocol variables
-	static uint8_t necCnt = 0;						// NEC bit counter
-	static NECCmd necCmd;							// NEC command
-	static NECState necState = STATE_NEC_IDLE;		// NEC decoding state
+    // NEC protocol variables
+    static uint8_t necCnt = 0;                      // NEC bit counter
+    static NECCmd necCmd;                           // NEC command
+    static NECState necState = STATE_NEC_IDLE;      // NEC decoding state
 
-	// RC5/RC6 protocol variables
-	static uint8_t rc5Cnt = 16;						// RC5 bit counter
-	static uint16_t rc5Cmd = 0;						// RC5 command
-	static RC5State rc5State = STATE_RC5_START1;	// RC5 decoding state
-	static int8_t rc6Cnt = 0;						// RC6 bit counter
-	static uint16_t rc6Cmd = 0;						// RC6 command
-	static RC5State rc6State = STATE_RC5_START1;	// RC6 decoding state
+    // RC5/RC6 protocol variables
+    static uint8_t rc5Cnt = 16;                     // RC5 bit counter
+    static uint16_t rc5Cmd = 0;                     // RC5 command
+    static RC5State rc5State = STATE_RC5_START1;    // RC5 decoding state
+    static int8_t rc6Cnt = 0;                       // RC6 bit counter
+    static uint16_t rc6Cmd = 0;                     // RC6 command
+    static RC5State rc6State = STATE_RC5_START1;    // RC6 decoding state
 
-	static uint8_t rc6TogBitOld = 0;
-	uint8_t rc6TogBit = 0;
+    static uint8_t rc6TogBitOld = 0;
+    uint8_t rc6TogBit = 0;
 
-	if (rcPin) {
-		// Try to decode as RC6 sequence
-		if (RC6_NEAR(delay, RC6_2T)) {
-			if (rc5State == STATE_RC5_START1) {
-				rc5State = STATE_RC5_MID1;
-				rc5Cnt--;
-				rc5Cmd <<= 1;
-				rc5Cmd |= 0x01;
-			} else if (rc5State == STATE_RC5_MID0) {
-				rc5State = STATE_RC5_START0;
-			}
-			if (rc6State == STATE_RC5_MID1) {
-				if (rc6Cnt == 21 || rc6Cnt == 16) {
-					rc6State = STATE_RC5_START1;
-				} else {
-					rc6State = STATE_RC5_MID0;
-					if (--rc6Cnt < 16)
-						rc6Cmd <<= 1;
-				}
-			} else if (rc6State == STATE_RC5_START0) {
-				if (rc6Cnt == 17) {
-					rc6State = STATE_RC5_MID0;
-					--rc6Cnt;
-					rc6TogBit = 0;
-				}
-			}
-		} else if (RC6_NEAR(delay, RC6_4T)) {
-			if (rc5State == STATE_RC5_MID0) {
-				rc5State = STATE_RC5_MID1;
-				rc5Cnt--;
-				rc5Cmd <<= 1;
-				rc5Cmd |= 0x01;
-			}
-		} else if (RC6_NEAR(delay, RC6_1T)) {
-			rc5Cnt = 13;								// Reset
-			rc5State = STATE_RC5_MID1;
-			if (rc6State == STATE_RC5_START0) {
-				rc6State = STATE_RC5_MID0;
-				if (--rc6Cnt < 16)
-					rc6Cmd <<= 1;
-			} else if (rc6State == STATE_RC5_MID1) {
-				rc6State = STATE_RC5_START1;
-			}
-		} else if (RC6_NEAR(delay, RC6_3T)) {
-			rc5Cnt = 13;								// Reset
-			rc5State = STATE_RC5_MID1;
-			if (rc6State == STATE_RC5_MID1) {
-				if (rc6Cnt == 17) {
-					rc6State = STATE_RC5_MID0;
-					--rc6Cnt;
-					rc6TogBit = 0;
-				} else if (rc6Cnt == 21 || rc6Cnt == 16) {
-					rc6State = STATE_RC5_MID0;
-					if (--rc6Cnt < 16) {
-						rc6Cmd <<= 1;
-					}
-				}
-			}
-		} else {
-			rc5Cnt = 13;								// Reset
-			rc5State = STATE_RC5_MID1;
-			rc6Cnt = 22;								// Reset
-			rc6State = STATE_RC5_START1;
-		}
+    if (rcPin) {
+        // Try to decode as RC6 sequence
+        if (RC6_NEAR(delay, RC6_2T)) {
+            if (rc5State == STATE_RC5_START1) {
+                rc5State = STATE_RC5_MID1;
+                rc5Cnt--;
+                rc5Cmd <<= 1;
+                rc5Cmd |= 0x01;
+            } else if (rc5State == STATE_RC5_MID0) {
+                rc5State = STATE_RC5_START0;
+            }
+            if (rc6State == STATE_RC5_MID1) {
+                if (rc6Cnt == 21 || rc6Cnt == 16) {
+                    rc6State = STATE_RC5_START1;
+                } else {
+                    rc6State = STATE_RC5_MID0;
+                    if (--rc6Cnt < 16)
+                        rc6Cmd <<= 1;
+                }
+            } else if (rc6State == STATE_RC5_START0) {
+                if (rc6Cnt == 17) {
+                    rc6State = STATE_RC5_MID0;
+                    --rc6Cnt;
+                    rc6TogBit = 0;
+                }
+            }
+        } else if (RC6_NEAR(delay, RC6_4T)) {
+            if (rc5State == STATE_RC5_MID0) {
+                rc5State = STATE_RC5_MID1;
+                rc5Cnt--;
+                rc5Cmd <<= 1;
+                rc5Cmd |= 0x01;
+            }
+        } else if (RC6_NEAR(delay, RC6_1T)) {
+            rc5Cnt = 13;                            // Reset
+            rc5State = STATE_RC5_MID1;
+            if (rc6State == STATE_RC5_START0) {
+                rc6State = STATE_RC5_MID0;
+                if (--rc6Cnt < 16)
+                    rc6Cmd <<= 1;
+            } else if (rc6State == STATE_RC5_MID1) {
+                rc6State = STATE_RC5_START1;
+            }
+        } else if (RC6_NEAR(delay, RC6_3T)) {
+            rc5Cnt = 13;                            // Reset
+            rc5State = STATE_RC5_MID1;
+            if (rc6State == STATE_RC5_MID1) {
+                if (rc6Cnt == 17) {
+                    rc6State = STATE_RC5_MID0;
+                    --rc6Cnt;
+                    rc6TogBit = 0;
+                } else if (rc6Cnt == 21 || rc6Cnt == 16) {
+                    rc6State = STATE_RC5_MID0;
+                    if (--rc6Cnt < 16) {
+                        rc6Cmd <<= 1;
+                    }
+                }
+            }
+        } else {
+            rc5Cnt = 13;                            // Reset
+            rc5State = STATE_RC5_MID1;
+            rc6Cnt = 22;                            // Reset
+            rc6State = STATE_RC5_START1;
+        }
 
-		// Try to decode as NEC sequence
-		if (necState == STATE_NEC_INIT) {
-			if (NEC_NEAR(delay, NEC_START)) {
-				necState = STATE_NEC_RECEIVE;
-			} else if (NEC_NEAR(delay, NEC_REPEAT) && ovfCnt < 2) {
-				irData.repeat = 1;
-				irData.ready = 1;
-				ovfCnt = 0;
-			}
-			necCnt = 0;
-		} else if (necState == STATE_NEC_RECEIVE) {
-			necCnt++;
-			necCmd.raw >>= 1;
-			if (NEC_NEAR(delay, NEC_ZERO))
-				necCmd.raw &= ~0x80000000;
-			else if (NEC_NEAR(delay, NEC_ONE))
-				necCmd.raw |= 0x80000000;
-			else
-				necCnt = 0;
-			if (necCnt == 32) {
-				if ((uint8_t)(~necCmd.ncmd) == necCmd.cmd) {
-					irData.ready = 1;
-					if (ovfCnt < 2)
-						irData.repeat = 1;
-					else
-						irData.repeat = 0;
-					irData.address = necCmd.laddr;
-					irData.command = necCmd.cmd;
-					ovfCnt = 0;
-				}
-			}
-		}
-	} else {
-		// Try to decode as RC6 sequence
-		if (RC6_NEAR(delay, RC6_2T)) {
-			if (rc6State == STATE_RC5_MID0) {
-				if (rc6Cnt == 16) {
-					rc6State = STATE_RC5_START0;
-				} else {
-					rc6State = STATE_RC5_MID1;
-					if (--rc6Cnt < 16) {
-						rc6Cmd <<= 1;
-						rc6Cmd |= 0x01;
-					}
-				}
-			} else if (rc6State == STATE_RC5_START1) {
-				if (rc6Cnt == 17) {
-					rc6State = STATE_RC5_MID1;
-					--rc6Cnt;
-					rc6TogBit = 1;
-				}
-			}
-			if (rc5State == STATE_RC5_MID1) {
-				rc5State = STATE_RC5_START1;
-			} else if (rc5State == STATE_RC5_START0) {
-				rc5State = STATE_RC5_MID0;
-				rc5Cnt--;
-				rc5Cmd <<= 1;
-			}
-		} else if (RC6_NEAR(delay, RC6_4T)) {
-			if (rc5State == STATE_RC5_MID1) {
-				rc5State = STATE_RC5_MID0;
-				rc5Cnt--;
-				rc5Cmd <<= 1;
-			}
-		} else if (RC6_NEAR(delay, RC6_1T)) {
-			if (rc6State == STATE_RC5_START1) {
-				rc6State = STATE_RC5_MID1;
-				if (--rc6Cnt < 16) {
-					rc6Cmd <<= 1;
-					rc6Cmd |= 0x01;
-				}
-			} else if (rc6State == STATE_RC5_MID0) {
-				rc6State = STATE_RC5_START0;
-			}
-		} else if (RC6_NEAR(delay, RC6_3T)) {
-			if (rc6State == STATE_RC5_MID0) {
-				if (rc6Cnt == 17) {
-					rc6State = STATE_RC5_MID1;
-					--rc6Cnt;
-					rc6TogBit = 1;
-				} else if (rc6Cnt == 16) {
-					rc6State = STATE_RC5_MID1;
-					rc6Cmd <<= 1;
-					rc6Cmd |= 0x01;
-				}
-			}
-		} else if (RC6_NEAR(delay, RC6_6T)) {
-			rc6State = STATE_RC5_MID1;
-			rc6Cnt = 21;
-		} else {
-			rc6Cnt = 22;								// Reset
-			rc6State = STATE_RC5_START1;
-		}
+        // Try to decode as NEC sequence
+        if (necState == STATE_NEC_INIT) {
+            if (NEC_NEAR(delay, NEC_START)) {
+                necState = STATE_NEC_RECEIVE;
+            } else if (NEC_NEAR(delay, NEC_REPEAT) && ovfCnt < 2) {
+                irData.repeat = 1;
+                irData.ready = 1;
+                ovfCnt = 0;
+            }
+            necCnt = 0;
+        } else if (necState == STATE_NEC_RECEIVE) {
+            necCnt++;
+            necCmd.raw >>= 1;
+            if (NEC_NEAR(delay, NEC_ZERO))
+                necCmd.raw &= ~0x80000000;
+            else if (NEC_NEAR(delay, NEC_ONE))
+                necCmd.raw |= 0x80000000;
+            else
+                necCnt = 0;
+            if (necCnt == 32) {
+                if ((uint8_t)(~necCmd.ncmd) == necCmd.cmd) {
+                    irData.ready = 1;
+                    if (ovfCnt < 2)
+                        irData.repeat = 1;
+                    else
+                        irData.repeat = 0;
+                    irData.address = necCmd.laddr;
+                    irData.command = necCmd.cmd;
+                    ovfCnt = 0;
+                }
+            }
+        }
+    } else {
+        // Try to decode as RC6 sequence
+        if (RC6_NEAR(delay, RC6_2T)) {
+            if (rc6State == STATE_RC5_MID0) {
+                if (rc6Cnt == 16) {
+                    rc6State = STATE_RC5_START0;
+                } else {
+                    rc6State = STATE_RC5_MID1;
+                    if (--rc6Cnt < 16) {
+                        rc6Cmd <<= 1;
+                        rc6Cmd |= 0x01;
+                    }
+                }
+            } else if (rc6State == STATE_RC5_START1) {
+                if (rc6Cnt == 17) {
+                    rc6State = STATE_RC5_MID1;
+                    --rc6Cnt;
+                    rc6TogBit = 1;
+                }
+            }
+            if (rc5State == STATE_RC5_MID1) {
+                rc5State = STATE_RC5_START1;
+            } else if (rc5State == STATE_RC5_START0) {
+                rc5State = STATE_RC5_MID0;
+                rc5Cnt--;
+                rc5Cmd <<= 1;
+            }
+        } else if (RC6_NEAR(delay, RC6_4T)) {
+            if (rc5State == STATE_RC5_MID1) {
+                rc5State = STATE_RC5_MID0;
+                rc5Cnt--;
+                rc5Cmd <<= 1;
+            }
+        } else if (RC6_NEAR(delay, RC6_1T)) {
+            if (rc6State == STATE_RC5_START1) {
+                rc6State = STATE_RC5_MID1;
+                if (--rc6Cnt < 16) {
+                    rc6Cmd <<= 1;
+                    rc6Cmd |= 0x01;
+                }
+            } else if (rc6State == STATE_RC5_MID0) {
+                rc6State = STATE_RC5_START0;
+            }
+        } else if (RC6_NEAR(delay, RC6_3T)) {
+            if (rc6State == STATE_RC5_MID0) {
+                if (rc6Cnt == 17) {
+                    rc6State = STATE_RC5_MID1;
+                    --rc6Cnt;
+                    rc6TogBit = 1;
+                } else if (rc6Cnt == 16) {
+                    rc6State = STATE_RC5_MID1;
+                    rc6Cmd <<= 1;
+                    rc6Cmd |= 0x01;
+                }
+            }
+        } else if (RC6_NEAR(delay, RC6_6T)) {
+            rc6State = STATE_RC5_MID1;
+            rc6Cnt = 21;
+        } else {
+            rc6Cnt = 22;                            // Reset
+            rc6State = STATE_RC5_START1;
+        }
 
-		// Try to decode as NEC sequence
-		if (NEC_NEAR(delay, NEC_PULSE) && necState != STATE_NEC_REPEAT) {
-			necState = STATE_NEC_RECEIVE;
-		} else if (NEC_NEAR(delay, NEC_INIT)) {
-			necState = STATE_NEC_INIT;
-			irData.type = IR_TYPE_NEC;
-		} else if (NEC_NEAR(delay, SAM_INIT)) {
-			necState = STATE_NEC_INIT;
-			irData.type = IR_TYPE_SAM;
-		} else {
-			necState = STATE_NEC_IDLE;
-		}
-	}
+        // Try to decode as NEC sequence
+        if (NEC_NEAR(delay, NEC_PULSE) && necState != STATE_NEC_REPEAT) {
+            necState = STATE_NEC_RECEIVE;
+        } else if (NEC_NEAR(delay, NEC_INIT)) {
+            necState = STATE_NEC_INIT;
+            irData.type = IR_TYPE_NEC;
+        } else if (NEC_NEAR(delay, SAM_INIT)) {
+            necState = STATE_NEC_INIT;
+            irData.type = IR_TYPE_SAM;
+        } else {
+            necState = STATE_NEC_IDLE;
+        }
+    }
 
-	if (rc5Cnt == 0 || rc6Cnt == 0) {
-		if (rc5Cnt == 0) {
-			rc6TogBit = (rc5Cmd & RC5_TOGB_MASK) != 0;
-			irData.type = IR_TYPE_RC5;
-			irData.address  = (rc5Cmd & RC5_ADDR_MASK) >> 6;
-			irData.command = (rc5Cmd & RC5_COMM_MASK) | (rc5Cmd & RC5_FIBT_MASK ? 0x00 : 0x40);
-		} else {
-			irData.type = IR_TYPE_RC6;
-			irData.address  = (rc6Cmd & RC6_ADDR_MASK) >> 8;
-			irData.command = rc6Cmd & RC6_COMM_MASK;
-		}
-		rc6Cnt = 22;
-		rc5Cnt = 13;
-		irData.ready = 1;
-		irData.repeat = (rc6TogBit == rc6TogBitOld);
-		rc6TogBitOld = rc6TogBit;
-	}
-	return;
+    if (rc5Cnt == 0 || rc6Cnt == 0) {
+        if (rc5Cnt == 0) {
+            rc6TogBit = (rc5Cmd & RC5_TOGB_MASK) != 0;
+            irData.type = IR_TYPE_RC5;
+            irData.address  = (rc5Cmd & RC5_ADDR_MASK) >> 6;
+            irData.command = (rc5Cmd & RC5_COMM_MASK) | (rc5Cmd & RC5_FIBT_MASK ? 0x00 : 0x40);
+        } else {
+            irData.type = IR_TYPE_RC6;
+            irData.address  = (rc6Cmd & RC6_ADDR_MASK) >> 8;
+            irData.command = rc6Cmd & RC6_COMM_MASK;
+        }
+        rc6Cnt = 22;
+        rc5Cnt = 13;
+        irData.ready = 1;
+        irData.repeat = (rc6TogBit == rc6TogBitOld);
+        rc6TogBitOld = rc6TogBit;
+    }
+    return;
 }
 
 IRData takeIrData()
 {
-	IRData ret = irData;
-	irData.ready = 0;
+    IRData ret = irData;
+    irData.ready = 0;
 
-	return ret;
+    return ret;
 }
 
 IRData getIrData()
 {
-	return irData;
+    return irData;
 }
 
 void setIrData(uint8_t type, uint8_t addr, uint8_t cmd)
 {
-	irData.type = type;
-	irData.address = addr;
-	irData.command = cmd;
+    irData.type = type;
+    irData.address = addr;
+    irData.command = cmd;
 
-	return;
+    return;
 }
--- remote.h
+++ remote.h
@@ -4,88 +4,88 @@
 #include <inttypes.h>
 
 // Time scale definitions and macroses
-#define RC_TIMER_DIV				4	// 1MHz / 250kHz of Timer 1 => delays in us
+#define RC_TIMER_DIV                4   // 1MHz / 250kHz of Timer 1 => delays in us
 
-#define NEC_DEV_MIN					0.7
-#define NEC_DEV_MAX					1.3
-#define NEC_MIN(delay)				((uint16_t)(delay / RC_TIMER_DIV * NEC_DEV_MIN))
-#define NEC_MAX(delay)				((uint16_t)(delay / RC_TIMER_DIV * NEC_DEV_MAX))
-#define NEC_NEAR(value, delay)		(value > NEC_MIN(delay) && value < NEC_MAX(delay))
+#define NEC_DEV_MIN                 0.7
+#define NEC_DEV_MAX                 1.3
+#define NEC_MIN(delay)              ((uint16_t)(delay / RC_TIMER_DIV * NEC_DEV_MIN))
+#define NEC_MAX(delay)              ((uint16_t)(delay / RC_TIMER_DIV * NEC_DEV_MAX))
+#define NEC_NEAR(value, delay)      (value > NEC_MIN(delay) && value < NEC_MAX(delay))
 
-#define RC6_DEV_MIN					0.8
-#define RC6_DEV_MAX					1.2
-#define RC6_MIN(delay)				((uint16_t)(delay / RC_TIMER_DIV * RC6_DEV_MIN))
-#define RC6_MAX(delay)				((uint16_t)(delay / RC_TIMER_DIV * RC6_DEV_MAX))
-#define RC6_NEAR(value, delay)		(value > RC6_MIN(delay) && value < RC6_MAX(delay))
+#define RC6_DEV_MIN                 0.8
+#define RC6_DEV_MAX                 1.2
+#define RC6_MIN(delay)              ((uint16_t)(delay / RC_TIMER_DIV * RC6_DEV_MIN))
+#define RC6_MAX(delay)              ((uint16_t)(delay / RC_TIMER_DIV * RC6_DEV_MAX))
+#define RC6_NEAR(value, delay)      (value > RC6_MIN(delay) && value < RC6_MAX(delay))
 
 // Remote control types
 enum {
-	IR_TYPE_RC5,
-	IR_TYPE_NEC,
-	IR_TYPE_RC6,
-	IR_TYPE_SAM,
+    IR_TYPE_RC5,
+    IR_TYPE_NEC,
+    IR_TYPE_RC6,
+    IR_TYPE_SAM,
 
-	IR_TYPE_NONE = 0x0F
+    IR_TYPE_NONE = 0x0F
 };
 
 // Structure for strore data received
 typedef struct {
-	uint8_t ready : 1;
-	uint8_t repeat : 1;
-	uint8_t rsvd1 : 1;
-	uint8_t rsvd2 : 1;
-	uint8_t type : 4;
-	uint8_t address;
-	uint8_t command;
+    uint8_t ready : 1;
+    uint8_t repeat : 1;
+    uint8_t rsvd1 : 1;
+    uint8_t rsvd2 : 1;
+    uint8_t type : 4;
+    uint8_t address;
+    uint8_t command;
 } IRData;
 
 // RC5/RC6 definitions
-#define RC6_1T						444
-#define RC6_2T						889
-#define RC6_3T						1333
-#define RC6_4T						1778
-#define RC6_6T						2667
+#define RC6_1T                      444
+#define RC6_2T                      889
+#define RC6_3T                      1333
+#define RC6_4T                      1778
+#define RC6_6T                      2667
 
-#define RC5_STBT_MASK				0x2000
-#define RC5_FIBT_MASK				0x1000
-#define RC5_TOGB_MASK				0x0800
-#define RC5_ADDR_MASK				0x07C0
-#define RC5_COMM_MASK				0x003F
+#define RC5_STBT_MASK               0x2000
+#define RC5_FIBT_MASK               0x1000
+#define RC5_TOGB_MASK               0x0800
+#define RC5_ADDR_MASK               0x07C0
+#define RC5_COMM_MASK               0x003F
 
-#define RC6_ADDR_MASK				0xFFC0
-#define RC6_COMM_MASK				0x00FF
+#define RC6_ADDR_MASK               0xFFC0
+#define RC6_COMM_MASK               0x00FF
 
 typedef enum {
-	STATE_RC5_MID0 = 0,
-	STATE_RC5_MID1,
-	STATE_RC5_START0,
-	STATE_RC5_START1,
+    STATE_RC5_MID0 = 0,
+    STATE_RC5_MID1,
+    STATE_RC5_START0,
+    STATE_RC5_START1,
 } RC5State;
 
 // NEC/Samsung definitions
-#define NEC_INIT					9000
-#define SAM_INIT					4500
-#define NEC_START					4500
-#define NEC_REPEAT					2250
-#define NEC_ZERO					560
-#define NEC_ONE						1680
-#define NEC_PULSE					560
+#define NEC_INIT                    9000
+#define SAM_INIT                    4500
+#define NEC_START                   4500
+#define NEC_REPEAT                  2250
+#define NEC_ZERO                    560
+#define NEC_ONE                     1680
+#define NEC_PULSE                   560
 
 typedef enum {
-	STATE_NEC_IDLE = 0,
-	STATE_NEC_INIT,
-	STATE_NEC_REPEAT,
-	STATE_NEC_RECEIVE,
+    STATE_NEC_IDLE = 0,
+    STATE_NEC_INIT,
+    STATE_NEC_REPEAT,
+    STATE_NEC_RECEIVE,
 } NECState;
 
 typedef union {
-	uint32_t raw;
-	struct {
-		uint8_t laddr;
-		uint8_t haddr;
-		uint8_t cmd;
-		uint8_t ncmd;
-	};
+    uint32_t raw;
+    struct {
+        uint8_t laddr;
+        uint8_t haddr;
+        uint8_t cmd;
+        uint8_t ncmd;
+    };
 } NECCmd;
 
 void rcInit(void);
@@ -94,4 +94,4 @@ IRData takeIrData(void);
 IRData getIrData(void);
 void setIrData(uint8_t type, uint8_t addr, uint8_t cmd);
 
-#endif /* REMOTE_H */
+#endif // REMOTE_H
